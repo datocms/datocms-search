adecd2a359efc3fda1e4f3f26a7414297fa06dc5
Stefano Verna <s.verna@cantierecreativo.net>
2017-05-19 17:48:29 +0200

    Uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-var path = require('path');
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'datocms-search.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new UglifyJSPlugin()
+  ]
 };
